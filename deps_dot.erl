9139f533417f5462ab45f7ee04b2d815382bec18
Francesco <f@mazzo.li>
2012-03-03 17:21:48 +0000

    Exclude modules

--- deps_dot.erl
+++ deps_dot.erl
@@ -1,48 +1,37 @@
 -module(deps_dot).
 
--export([modules/1, modules/2]).
+-export([modules/2, modules/3]).
 
 -include("deps.hrl").
--import(deps_misc, [format/2]).
+-import(deps_misc, [format/2, shuffle/1]).
 
--spec modules(deps_graph()) -> string().
--spec modules(deps_graph(), string()) -> 'ok'.
+-spec modules(deps_graph(), [atom()]) -> string().
+-spec modules(deps_graph(), [atom()], string()) -> 'ok'.
 
-modules(Graph) ->
-    Names = lists:map(
-              fun ({#module_node {name = Name}, _}) -> Name end,
-              Graph),
-    "digraph callraph {~n" ++ modules1(Graph, label(Names)) ++ "}".
+modules(Graph, Exclude) ->
+    "digraph callraph {~n"
+        "concentrate=true~n" ++
+        lists:append(modules1(Graph, Exclude, Graph)) ++ "}".
 
-modules(Graph, Filepath) ->
-    deps_misc:write_to_file(modules(Graph), Filepath).
+modules(Graph, Exclude, Filepath) ->
+    deps_misc:write_to_file(modules(Graph, Exclude), Filepath).
 
 %% ----
 
-modules1([], _Labels) ->
+modules1([], _E, _G) ->
     "";
-modules1([{#module_node { name = Name,
-                          to   = To,
-                          from = _From
-                        }, _} | Rest], Labels) ->
-    Label = gb_trees:get(Name, Labels),
-    format("\t~B [label=\"~p\"]~n", [Label, Name]) ++
-        lists:flatmap(
-          fun(M) ->
-                  %% We don't graph external modules
-                  case gb_trees:lookup(M, Labels) of
-                      none ->
-                          "";
-                      {value, Label1} ->
-                          format("\t~B -> ~B~n", [Label, Label1])
-                  end
-          end, To) ++
-        modules1(Rest, Labels).
-
-
-label(Graph) ->
-    {_, Labels} = lists:foldl(
-                    fun(El, {C, M}) ->
-                            {C+1, gb_trees:insert(El, C, M)}
-                    end, {1, gb_trees:empty()}, Graph),
-    Labels.
+modules1([{#module_node {name = Name, to = To}, _} | Rest], Exclude, Graph) ->
+    case lists:member(Name, Exclude) of
+        true ->
+            modules1(Rest, Exclude, Graph);
+        false ->
+            lists:map(
+              fun(M) ->
+                      %% We don't graph external modules
+                      case lists:member(M, Exclude) orelse
+                  deps_graph:lookup(M, Graph) =:= none of
+                          true  -> "";
+                          false -> format("\t~p -> ~p~n", [Name, M])
+                      end
+              end, To) ++ modules1(Rest, Exclude, Graph)
+    end.
